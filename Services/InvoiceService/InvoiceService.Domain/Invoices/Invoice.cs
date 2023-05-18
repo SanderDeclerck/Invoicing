3acf72fee5acc167cfba9a9cc3330ce6d9ed6f4d
Sander Declerck <sander@sdsoftware.be>
2023-05-18 19:53:54 +0000

    Move InvoiceTotals to seperate file

--- Services/InvoiceService/InvoiceService.Domain/Invoices/Invoice.cs
+++ Services/InvoiceService/InvoiceService.Domain/Invoices/Invoice.cs
@@ -52,26 +52,3 @@ public class Invoice : EntityBase, IAggregateRoot
 
     public InvoiceTotals CalculateTotal() => new InvoiceTotals(InvoiceLines);
 }
-
-public class InvoiceTotals
-{
-    public InvoiceTotals(InvoiceLineCollection lines)
-    {
-        decimal totalIncludingVat = 0, totalExcludingVat = 0, totalVat = 0;
-
-        foreach (var line in lines.Items) 
-        {
-            totalIncludingVat += line.TotalIncludingVat;
-            totalExcludingVat += line.TotalExcludingVat;
-            totalVat += line.VatAmount;
-        }
-
-        TotalIncludingVat = totalIncludingVat;
-        TotalExcludingVat = totalExcludingVat;
-        TotalVat = totalVat;
-    }
-
-    public decimal TotalIncludingVat { get; }
-    public decimal TotalExcludingVat { get; }
-    public decimal TotalVat { get; }
-}
