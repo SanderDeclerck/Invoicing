78a9661c95dcd46954303d4277507558351a34b0
Sander Declerck <sander@sdsoftware.be>
2023-11-03 17:07:03 +0000

    Add UserService Web project

--- Services/AuthenticationService/AuthenticationService.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthenticationService.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
